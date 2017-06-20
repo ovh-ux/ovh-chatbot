8f714db88a4f35f2cdaf46dcebb6b2f710fb0364
Julien DONQUE <julien.donque@corp.ovh.com>
2017-06-20 17:02:22 +0200

    feat(ndh): add more questions

--- bots/messageTypes/message/ndh_question.js
+++ bots/messageTypes/message/ndh_question.js
@@ -4,18 +4,17 @@ const Bluebird = require("bluebird");
 const { TextMessage } = require("../../../platforms/generics");
 
 const questions = [
-  "Pourrais tu nous dire combien fait la multiplication de ces 3 chiffres. Le nombre de datacenters chez OVH, le nombre de pays dans lesquels nous sommes implantés et l'âge de la société OVH ?",
-  "Quel est le code de la couleur du logo de OVH ?",
-  "Ou se situe le siege social de OVH ?",
-  "Quel est le produit du nombre de langue disponibles sur le site ovh.com et de la fréquence maximale de nos CPU dans nos serveurs dédiés ?",
-  "Que veux dire OVH ?",
+  "Pourrais-tu nous dire combien fait la multiplication de ces 3 chiffres. Le nombre de datacenters chez OVH, le nombre de pays dans lesquels nous sommes implantés et l'âge de la société OVH ?",
+  "Quel est le code de la couleur du logo d'OVH ?",
+  "Où se situe le siège social d'OVH ?",
+  "Quel est le produit du nombre de langues disponibles sur le site ovh.com ?",
+  "Que veut dire OVH ?",
   "Quelle est ma version ? Petit indice: je suis open source",
-  "Qui est mon créateur original ?",
-  "Combien de partenaires technique OVH a ?",
-  "Combien de temps faut il pour lire l'article sur moi ? Petit indice: l'article se trouve sur le blog de OVH",
-  "Combien y a t'il de questions en tout ? :p"
+  "Combien j'ai de contributeurs (sur github)?",
+  "Combien de partenaires techniques possède OVH ? indice: https://www.ovh.com/fr/apropos/partenaires-techniques.xml",
+  "Combien de temps faut-il pour lire l'article sur mon développement ? Petit indice: l'article se trouve sur le blog d'OVH",
+  "Combien de questions différentes je possède ? Petit indice: je suis open source 🤗"
 ];
-
 const base = "Salut à toi 👋, tu as soif ? 🍺 Il faut la mériter si tu en veux. ";
 
 class NdhQuestion {
@@ -24,4 +23,4 @@ class NdhQuestion {
   }
 }
 
-module.exports = {ndh_Question: NdhQuestion};
+module.exports = {ndh_question: NdhQuestion};
