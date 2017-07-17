821fe2d3caaae675b6ca3543e1d4a24abcfffd44
Julien DONQUE <julien.donque@corp.ovh.com>
2017-07-17 13:02:07 +0200

    feat(bot): add status

--- bots/messageTypes/message/status_update.js
+++ bots/messageTypes/message/status_update.js
@@ -1,7 +1,8 @@
 "use strict";
 
-const Bluebird = require("Bluebird");
-const utils = require("../utils");
+const Bluebird = require("bluebird");
+const utils = require("../../utils");
+const { ListItem, CardMessage, TextMessage } = require("../../../platforms/generics");
 
 // const responsesCst = require("../../constants/responses").FR;
 
@@ -10,26 +11,45 @@ class StatusUpdate {
   static action (senderId) {
     return utils.getOvhClient(senderId)
       .then((ovhClient) => Bluebird.props({
-        clouddStatus: ovhClient.requestPromised("GET", "/status/incident").filter((incident) => incident.status !== "finished"),
-        xdslStatus: getXdslStatus(ovhClient),
-        hostingStatus: ovhClient.requestPromised("GET", "/hosting/web/incident")
+        cloudStatus: ovhClient.requestPromised("GET", "/status/task"),
+        xdslStatus: getXdslStatus(ovhClient)
       }))
-      .then(({ cloudStatus, xdslStatus, hostingStatus }) => {
-        console.log(cloudStatus, xdslStatus, hostingStatus);
+      .then(({ cloudStatus, xdslStatus }) => {
+        let responses = [
+          ...cloudStatus.map((cloudIncident) => new CardMessage([
+            new ListItem("Incident", cloudIncident.title),
+            new ListItem("Etat", `${cloudIncident.status} (${cloudIncident.progress}%)`),
+            new ListItem("Details", cloudIncident.details)
+          ], true)),
+          ...xdslStatus.map((xdslIncident) => new CardMessage([
+            new ListItem("Incident", xdslIncident.comment),
+            new ListItem("Résolution", xdslIncident.endDate),
+            new ListItem("Details", `http://travaux.ovh.net/?do=details&id=${xdslIncident.taskId}`)
+          ], true))
+        ];
+
+        if (!responses.length) {
+          responses = [new TextMessage("Aucun probleme, tout est ok !")];
+        }
+
+        console.log(responses);
+
+        return { responses, feedback: false };
       });
   }
 }
 
 function getXdslStatus (ovhClient) {
   return ovhClient.requestPromised("GET", "/xdsl")
-    .map((id) => ovhClient.requestPromised("GET", `/xdsl/${id}/incident`).catch((err) => {
+    .map((service) => ovhClient.requestPromised("GET", `/xdsl/${service}/incidents`).catch((err) => {
       if (err.error === 404 || err.statusCode === 404) {
         return null;
       }
 
       return Bluebird.reject(err);
     }))
-    .filter((incident) => incident != null);
+    .filter((incident) => incident != null)
+    .map((incidentId) => ovhClient.requestPromised("GET", `/xdsl/incidents/${incidentId}`));
 }
 
 module.exports = { status_update: StatusUpdate };
