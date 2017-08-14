f7764251f77ea2b60b897ec27d9506e41b18e879
Julien DONQUE <julien.donque@corp.ovh.com>
2017-08-14 11:53:16 +0200

    chore(express): remove useless lusca (bc no sessions)

--- config/express.js
+++ config/express.js
@@ -11,7 +11,6 @@ const mongo = require("../providers/orm/nosql/mongo");
 const verifyRequestSignature = require("./middlewares/verifyRequest");
 const requestLogger = require("../providers/logging/request");
 const utilsMiddleware = require("./middlewares/utils");
-const lusca = require("lusca");
 const cookieParser = require("cookie-parser");
 const verifyOvhUser = require("./middlewares/verifyOvhUser");
 
@@ -79,22 +78,6 @@ module.exports = function (config) {
     const errorApi = res.error(404, `${req.originalUrl} doesn't exist`);
     return res.status(errorApi.statusCode).json(errorApi);
   });
-
-  if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "preproduction") {
-    app.use(
-      lusca({
-        csrf: true,
-        xframe: "SAMEORIGIN",
-        hsts: {
-          maxAge: 31536000, // 1 year, in seconds
-          includeSubDomains: true,
-          preload: true
-        },
-        xssProtection: true,
-        nosniff: true
-      })
-    );
-  }
 
   Object.keys(routes).forEach((key) => routes[key](api));
 
